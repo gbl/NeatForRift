76f8237ccc3be0e4c17cf067975e5bfbb5873229
gbui <gbui@users.noreply.github.com>
2018-08-25 15:45:18 -0700

    Fix mixin bootstrap exception and add CurseGradle

--- src/main/java/vazkii/neat/NeatCore.java
+++ src/main/java/vazkii/neat/NeatCore.java
@@ -1,11 +1,13 @@
 package vazkii.neat;
 
 import org.dimdev.riftloader.listener.InitializationListener;
+import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
 public class NeatCore implements InitializationListener {
     @Override
     public void onInitialization() {
+        MixinBootstrap.init();
         Mixins.addConfiguration("mixins.neat.json");
     }
 }
